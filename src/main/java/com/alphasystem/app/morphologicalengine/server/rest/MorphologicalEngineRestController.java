b178ecc1f5a2a155e0d270229a4310e20dcc024f
sfali23 <f.syed.ali@gmail.com>
2017-04-30 15:37:51 -0400

    updated

--- src/main/java/com/alphasystem/app/morphologicalengine/server/rest/MorphologicalEngineRestController.java
+++ src/main/java/com/alphasystem/app/morphologicalengine/server/rest/MorphologicalEngineRestController.java
@@ -56,159 +56,159 @@ import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
 @RequestMapping("/morphologicalEngine")
 public class MorphologicalEngineRestController {
 
-    @Autowired private ConjugationBuilder conjugationBuilder;
-    @Autowired private AbbreviatedConjugationBuilder abbreviatedConjugationBuilder;
-    @Autowired private DetailedConjugationBuilder detailedConjugationBuilder;
-    @Autowired private MorphologicalChartEngineFactory morphologicalChartEngineFactory;
+	@Autowired private ConjugationBuilder conjugationBuilder;
+	@Autowired private AbbreviatedConjugationBuilder abbreviatedConjugationBuilder;
+	@Autowired private DetailedConjugationBuilder detailedConjugationBuilder;
+	@Autowired private MorphologicalChartEngineFactory morphologicalChartEngineFactory;
 
-    @CrossOrigin("*")
-    @RequestMapping(value = "/morphologicalChart/format/{format}", method = RequestMethod.POST, consumes = APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Object> morphologicalChart(@PathVariable(name = "format") OutputFormat format,
-                                                     @RequestBody ConjugationTemplate conjugationTemplate) throws Exception {
-        ResponseEntity<Object> result;
-        if (OutputFormat.STREAM.equals(format)) {
-            return morphologicalChart2(conjugationTemplate);
-        } else {
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Access-Control-Allow-Methods", "POST");
-            headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+	@CrossOrigin("*")
+	@RequestMapping(value = "/morphologicalChart/format/{format}", method = RequestMethod.POST, consumes = APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<Object> morphologicalChart(@PathVariable(name = "format") OutputFormat format,
+													 @RequestBody ConjugationTemplate conjugationTemplate) throws Exception {
+		ResponseEntity<Object> result;
+		if (OutputFormat.STREAM.equals(format)) {
+			return morphologicalChart2(conjugationTemplate);
+		} else {
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Access-Control-Allow-Methods", "POST");
+			headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
 
-            List<MorphologicalChart> morphologicalCharts = new ArrayList<>();
-            final List<ConjugationData> conjugationDataList = conjugationTemplate.getData();
-            conjugationDataList.forEach(conjugationData ->
-                    morphologicalCharts.add(conjugationBuilder.doConjugation(getConjugationRoots(conjugationData), format)));
-            result = ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_JSON_UTF8).body(morphologicalCharts);
-        }
-        return result;
-    }
+			List<MorphologicalChart> morphologicalCharts = new ArrayList<>();
+			final List<ConjugationData> conjugationDataList = conjugationTemplate.getData();
+			conjugationDataList.forEach(conjugationData ->
+					morphologicalCharts.add(conjugationBuilder.doConjugation(getConjugationRoots(conjugationData), format)));
+			result = ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_JSON_UTF8).body(morphologicalCharts);
+		}
+		return result;
+	}
 
-    @CrossOrigin("*")
-    @RequestMapping(value = "/morphologicalChart/format/STREAM", method = RequestMethod.POST,
-            produces = APPLICATION_OCTET_STREAM_VALUE, consumes = APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Object> morphologicalChart2(@RequestBody ConjugationTemplate conjugationTemplate)
-            throws Exception {
-        final Path tempFile = Files.createTempFile(AppUtil.USER_TEMP_DIR.toPath(), conjugationTemplate.getId(), ".docx");
-        MorphologicalChartEngine engine = morphologicalChartEngineFactory.createMorphologicalChartEngine(conjugationTemplate);
-        engine.createDocument(tempFile);
+	@CrossOrigin("*")
+	@RequestMapping(value = "/morphologicalChart/format/STREAM", method = RequestMethod.POST,
+			produces = APPLICATION_OCTET_STREAM_VALUE, consumes = APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<Object> morphologicalChart2(@RequestBody ConjugationTemplate conjugationTemplate)
+			throws Exception {
+		final Path tempFile = Files.createTempFile(AppUtil.USER_TEMP_DIR.toPath(), conjugationTemplate.getId(), ".docx");
+		MorphologicalChartEngine engine = morphologicalChartEngineFactory.createMorphologicalChartEngine(conjugationTemplate);
+		engine.createDocument(tempFile);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Access-Control-Allow-Methods", "POST");
-        headers.add("Access-Control-Allow-Headers", "Content-Type");
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        // headers.add("Access-Control-Allow-Origin", "http://localhost:4200");
-        headers.add("Content-Disposition", "filename=" + tempFile.getFileName());
-        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        headers.add("Pragma", "no-cache");
-        headers.add("Expires", "0");
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Access-Control-Allow-Methods", "POST");
+		headers.add("Access-Control-Allow-Headers", "Content-Type");
+		headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+		// headers.add("Access-Control-Allow-Origin", "http://localhost:4200");
+		headers.add("Content-Disposition", "filename=" + tempFile.getFileName());
+		headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		headers.add("Pragma", "no-cache");
+		headers.add("Expires", "0");
 
-        FileSystemResource docxFile = new FileSystemResource(tempFile.toFile());
-        return ResponseEntity.ok().contentLength(docxFile.contentLength()).headers(headers)
-                .contentType(MediaType.APPLICATION_OCTET_STREAM).body(new InputStreamResource(docxFile.getInputStream()));
-    }
+		FileSystemResource docxFile = new FileSystemResource(tempFile.toFile());
+		return ResponseEntity.ok().contentLength(docxFile.contentLength()).headers(headers)
+				.contentType(MediaType.APPLICATION_OCTET_STREAM).body(new InputStreamResource(docxFile.getInputStream()));
+	}
 
-    @CrossOrigin("*")
-    @RequestMapping(value = "/AbbreviatedConjugation/template/{template}/format/{format}", method = RequestMethod.GET,
-            consumes = APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<AbbreviatedConjugation> doAbbreviatedConjugation(
-            @PathVariable(name = "template") NamedTemplate template,
-            @PathVariable(name = "format") OutputFormat format,
-            @RequestHeader(name = "firstRadical") ArabicLetterType firstRadical,
-            @RequestHeader(name = "secondRadical") ArabicLetterType secondRadical,
-            @RequestHeader(name = "thirdRadical") ArabicLetterType thirdRadical,
-            @RequestHeader(name = "fourthRadical", required = false) ArabicLetterType fourthRadical,
-            @RequestHeader(name = "verbalNouns", required = false) VerbalNoun[] verbalNouns,
-            @RequestHeader(name = "translation", required = false) String translation,
-            @RequestHeader(name = "skipRuleProcessing", required = false) boolean skipRuleProcessing,
-            @RequestHeader(name = "removePassiveLine", required = false) boolean removePassiveLine) {
-        RootLetters rootLetters = new RootLetters(firstRadical, secondRadical, thirdRadical, fourthRadical);
+	@CrossOrigin("*")
+	@RequestMapping(value = "/AbbreviatedConjugation/template/{template}/format/{format}", method = RequestMethod.GET,
+			consumes = APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<AbbreviatedConjugation> doAbbreviatedConjugation(
+			@PathVariable(name = "template") NamedTemplate template,
+			@PathVariable(name = "format") OutputFormat format,
+			@RequestHeader(name = "firstRadical") ArabicLetterType firstRadical,
+			@RequestHeader(name = "secondRadical") ArabicLetterType secondRadical,
+			@RequestHeader(name = "thirdRadical") ArabicLetterType thirdRadical,
+			@RequestHeader(name = "fourthRadical", required = false) ArabicLetterType fourthRadical,
+			@RequestHeader(name = "verbalNouns", required = false) VerbalNoun[] verbalNouns,
+			@RequestHeader(name = "translation", required = false) String translation,
+			@RequestHeader(name = "skipRuleProcessing", required = false) boolean skipRuleProcessing,
+			@RequestHeader(name = "removePassiveLine", required = false) boolean removePassiveLine) {
+		RootLetters rootLetters = new RootLetters(firstRadical, secondRadical, thirdRadical, fourthRadical);
 
-        NounRootBase[] verbalNounRootBases = null;
-        if(!ArrayUtils.isEmpty(verbalNouns)){
-            verbalNounRootBases = VerbalNounFactory.getByVerbalNouns(Arrays.asList(verbalNouns));
-        }
-        final ConjugationRoots conjugationRoots = ConjugationHelper.getConjugationRoots(template, rootLetters, translation,
-                verbalNounRootBases, null);
-        final AbbreviatedConjugation abbreviatedConjugation = abbreviatedConjugationBuilder.doAbbreviatedConjugation(conjugationRoots, format);
+		NounRootBase[] verbalNounRootBases = null;
+		if (!ArrayUtils.isEmpty(verbalNouns)) {
+			verbalNounRootBases = VerbalNounFactory.getByVerbalNouns(Arrays.asList(verbalNouns));
+		}
+		final ConjugationRoots conjugationRoots = ConjugationHelper.getConjugationRoots(template, rootLetters, translation,
+				verbalNounRootBases, null);
+		final AbbreviatedConjugation abbreviatedConjugation = abbreviatedConjugationBuilder.doAbbreviatedConjugation(conjugationRoots, format);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Access-Control-Allow-Methods", "GET");
-        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_JSON_UTF8).body(abbreviatedConjugation);
-    }
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Access-Control-Allow-Methods", "GET");
+		headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_JSON_UTF8).body(abbreviatedConjugation);
+	}
 
-    @CrossOrigin("*")
-    @RequestMapping(value = "/AbbreviatedConjugation/format/{format}", method = RequestMethod.POST,
-            consumes = APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<AbbreviatedConjugation[]> doAbbreviatedConjugations(
-            @PathVariable(name = "format") OutputFormat format,
-            @RequestBody ConjugationTemplate conjugationTemplate){
+	@CrossOrigin("*")
+	@RequestMapping(value = "/AbbreviatedConjugation/format/{format}", method = RequestMethod.POST,
+			consumes = APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<AbbreviatedConjugation[]> doAbbreviatedConjugations(
+			@PathVariable(name = "format") OutputFormat format,
+			@RequestBody ConjugationTemplate conjugationTemplate) {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Access-Control-Allow-Methods", "POST");
-        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Access-Control-Allow-Methods", "POST");
+		headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
 
-        List<AbbreviatedConjugation> abbreviatedConjugations = new ArrayList<>();
-        final List<ConjugationData> conjugationDataList = conjugationTemplate.getData();
+		List<AbbreviatedConjugation> abbreviatedConjugations = new ArrayList<>();
+		final List<ConjugationData> conjugationDataList = conjugationTemplate.getData();
 
-        conjugationDataList.forEach(conjugationData ->
-                abbreviatedConjugations.add(abbreviatedConjugationBuilder.doAbbreviatedConjugation(getConjugationRoots(conjugationData), format)));
-        final AbbreviatedConjugation[] conjugations = abbreviatedConjugations.toArray(new AbbreviatedConjugation[abbreviatedConjugations.size()]);
-        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_JSON_UTF8).body(conjugations);
-    }
+		conjugationDataList.forEach(conjugationData ->
+				abbreviatedConjugations.add(abbreviatedConjugationBuilder.doAbbreviatedConjugation(getConjugationRoots(conjugationData), format)));
+		final AbbreviatedConjugation[] conjugations = abbreviatedConjugations.toArray(new AbbreviatedConjugation[abbreviatedConjugations.size()]);
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_JSON_UTF8).body(conjugations);
+	}
 
-    @CrossOrigin("*")
-    @RequestMapping(value = "/DetailedConjugation/type/{type}/template/{template}/format/{format}", method = RequestMethod.GET,
-            consumes = APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<ConjugationGroup[]> doDetailedConjugation(
-            @PathVariable(name = "type") SarfTermType sarfTermType,
-            @PathVariable(name = "template") NamedTemplate template,
-            @PathVariable(name = "format") OutputFormat format,
-            @RequestHeader(name = "firstRadical") ArabicLetterType firstRadical,
-            @RequestHeader(name = "secondRadical") ArabicLetterType secondRadical,
-            @RequestHeader(name = "thirdRadical") ArabicLetterType thirdRadical,
-            @RequestHeader(name = "fourthRadical", required = false) ArabicLetterType fourthRadical,
-            @RequestHeader(name = "verbalNouns", required = false) VerbalNoun[] verbalNouns,
-            @RequestHeader(name = "skipRuleProcessing", required = false) boolean skipRuleProcessing) {
+	@CrossOrigin("*")
+	@RequestMapping(value = "/DetailedConjugation/type/{type}/template/{template}/format/{format}", method = RequestMethod.GET,
+			consumes = APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<ConjugationGroup[]> doDetailedConjugation(
+			@PathVariable(name = "type") SarfTermType sarfTermType,
+			@PathVariable(name = "template") NamedTemplate template,
+			@PathVariable(name = "format") OutputFormat format,
+			@RequestHeader(name = "firstRadical") ArabicLetterType firstRadical,
+			@RequestHeader(name = "secondRadical") ArabicLetterType secondRadical,
+			@RequestHeader(name = "thirdRadical") ArabicLetterType thirdRadical,
+			@RequestHeader(name = "fourthRadical", required = false) ArabicLetterType fourthRadical,
+			@RequestHeader(name = "verbalNouns", required = false) VerbalNoun[] verbalNouns,
+			@RequestHeader(name = "skipRuleProcessing", required = false) boolean skipRuleProcessing) {
 
-        RootLetters rootLetters = new RootLetters(firstRadical, secondRadical, thirdRadical, fourthRadical);
-        final String templateId = String.format("%s_%s_%s", sarfTermType.name(), template.name(), rootLetters.getName());
-        ConjugationGroup[] groups = doConjugate(sarfTermType, template, format, skipRuleProcessing, rootLetters, verbalNouns);
+		RootLetters rootLetters = new RootLetters(firstRadical, secondRadical, thirdRadical, fourthRadical);
+		final String templateId = String.format("%s_%s_%s", sarfTermType.name(), template.name(), rootLetters.getName());
+		ConjugationGroup[] groups = doConjugate(sarfTermType, template, format, skipRuleProcessing, rootLetters, verbalNouns);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Access-Control-Allow-Methods", "GET");
-        headers.set("templateId", templateId);
-        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Access-Control-Allow-Methods", "GET");
+		headers.set("templateId", templateId);
+		headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
 
-        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_JSON_UTF8).body(groups);
-    }
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_JSON_UTF8).body(groups);
+	}
 
-    private ConjugationGroup[] doConjugate(SarfTermType sarfTermType, NamedTemplate template, OutputFormat format,
-                                           boolean skipRuleProcessing, RootLetters rootLetters, VerbalNoun[] verbalNouns) {
-        ConjugationGroup[] groups = null;
-        final Form form = Form.getByTemplate(template);
-        switch (sarfTermType) {
-            case PAST_TENSE:
-            case PRESENT_TENSE:
-            case PAST_PASSIVE_TENSE:
-            case PRESENT_PASSIVE_TENSE:
-            case IMPERATIVE:
-            case FORBIDDING:
-                final VerbRootBase[] verbRootBases = ConjugationHelper.getVerbRootBases(sarfTermType, form);
-                groups = detailedConjugationBuilder.doConjugate(sarfTermType, template, rootLetters, verbRootBases,
-                        skipRuleProcessing, format);
-                break;
-            case ACTIVE_PARTICIPLE_MASCULINE:
-            case ACTIVE_PARTICIPLE_FEMININE:
-            case PASSIVE_PARTICIPLE_MASCULINE:
-            case PASSIVE_PARTICIPLE_FEMININE:
-            case VERBAL_NOUN:
-            case NOUN_OF_PLACE_AND_TIME:
-                NounRootBase[] nounRootBases = ConjugationHelper.getNounRootBase(sarfTermType, form, verbalNouns);
-                groups = detailedConjugationBuilder.doConjugate(sarfTermType, template, rootLetters, nounRootBases,
-                        skipRuleProcessing, format);
-                break;
-        }
-        return groups;
-    }
+	private ConjugationGroup[] doConjugate(SarfTermType sarfTermType, NamedTemplate template, OutputFormat format,
+										   boolean skipRuleProcessing, RootLetters rootLetters, VerbalNoun[] verbalNouns) {
+		ConjugationGroup[] groups = null;
+		final Form form = Form.getByTemplate(template);
+		switch (sarfTermType) {
+			case PAST_TENSE:
+			case PRESENT_TENSE:
+			case PAST_PASSIVE_TENSE:
+			case PRESENT_PASSIVE_TENSE:
+			case IMPERATIVE:
+			case FORBIDDING:
+				final VerbRootBase[] verbRootBases = ConjugationHelper.getVerbRootBases(sarfTermType, form);
+				groups = detailedConjugationBuilder.doConjugate(sarfTermType, template, rootLetters, verbRootBases,
+						skipRuleProcessing, format);
+				break;
+			case ACTIVE_PARTICIPLE_MASCULINE:
+			case ACTIVE_PARTICIPLE_FEMININE:
+			case PASSIVE_PARTICIPLE_MASCULINE:
+			case PASSIVE_PARTICIPLE_FEMININE:
+			case VERBAL_NOUN:
+			case NOUN_OF_PLACE_AND_TIME:
+				NounRootBase[] nounRootBases = ConjugationHelper.getNounRootBase(sarfTermType, form, verbalNouns);
+				groups = detailedConjugationBuilder.doConjugate(sarfTermType, template, rootLetters, nounRootBases,
+						skipRuleProcessing, format);
+				break;
+		}
+		return groups;
+	}
 
 }
